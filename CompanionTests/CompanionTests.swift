1ae04d9d1938b8982b79d257ec71deffe2fb5f3e
Selena <auraselena@icloud.com>
2025-04-11 14:34:52 +0800

    Initial Commit

---
+++ CompanionTests/CompanionTests.swift
@@ -0,0 +1,17 @@
+//
+//  CompanionTests.swift
+//  CompanionTests
+//
+//  Created by Selena Aura on 11/04/25.
+//
+
+import Testing
+@testable import Companion
+
+struct CompanionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
