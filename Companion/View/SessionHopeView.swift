08a95ecd50811cf82a8365b4b285b5ec3b93a70d
auraselena <auraselena_ph@gmail.com>
2025-04-15 15:08:44 +0800

    - rename some Views - finalize UI and navigation between Views

---
+++ Companion/View/SessionHopeView.swift
@@ -0,0 +1,18 @@
+//
+//  SessionHopeView.swift
+//  Companion
+//
+//  Created by Selena Aura on 15/04/25.
+//
+
+import SwiftUI
+
+struct SessionHopeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SessionHopeView()
+}
---
+++ Companion/View/VideoCallView.swift
@@ -0,0 +1,18 @@
+//
+//  VideoCallView.swift
+//  Companion
+//
+//  Created by Selena Aura on 15/04/25.
+//
+
+import SwiftUI
+
+struct VideoCallView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VideoCallView()
+}
