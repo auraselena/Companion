ca302126d41033213c6b9321db2d80e1e90497db
auraselena <auraselena_ph@gmail.com>
2025-04-24 16:06:42 +0800

    - implement access request for camera and microphone - add functionality to record video from main camera/front camera, as an alternative of video call feature

---
+++ Companion/View Model/CameraViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  CameraViewModel.swift
+//  Companion
+//
+//  Created by Selena Aura on 21/04/25.
+//
+
+import SwiftUI
+
+struct CameraViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CameraViewModel()
+}
---
+++ Companion/View/CameraPreview.swift
@@ -0,0 +1,18 @@
+//
+//  CameraPreview.swift
+//  Companion
+//
+//  Created by Selena Aura on 21/04/25.
+//
+
+import SwiftUI
+
+struct CameraPreview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CameraPreview()
+}
